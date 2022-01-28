aa963cd8232077bd1163afe98a6a37f46da9ca71
Mr-Yeety <98505470+Mr-Yeety@users.noreply.github.com>
2022-01-27 20:47:35 -0700

    Add files via upload
    Made basic User class

--- user.py
+++ user.py
@@ -1,1 +1,22 @@
-print("Hello User")
+class User():
+    
+    def __init__(self, name, address, id):
+        self.name = name
+        self.address = address
+        self.id = id
+    
+    def verify(self):
+        id = 6969420
+
+        if self.id != id:
+            return False
+        else:
+            return True
+    
+
+
+
+
+
+
+
