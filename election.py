be93a1f80dde23181dc768609fb9f277c28edb0a
Mr-Yeety <98505470+Mr-Yeety@users.noreply.github.com>
2022-01-27 20:53:07 -0700

    Add files via upload
    New Election Class

---
+++ election.py
@@ -0,0 +1,8 @@
+class Election():
+     
+    def __init__(self, people_running,num_of_people ):
+        self.people_running = people_running
+        self.num_of_people = num_of_people
+    
+    def vote()
+
